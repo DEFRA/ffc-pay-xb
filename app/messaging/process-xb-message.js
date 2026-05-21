069b65cd52210baacd3feda328555fe2821b595e
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2026-05-21 17:02:19 +0100

    reduce logs (#32)
    * reduce logs
    
    * 1.1.17
    
    * update version bump and override
    
    * review fixes + added race condition healthcheck
    
    ---------
    
    Co-authored-by: ffcplatform <ffcplatforms@environment-agency.gov.uk>

--- app/messaging/process-xb-message.js
+++ app/messaging/process-xb-message.js
@@ -1,11 +1,10 @@
-const util = require('util')
 const { convertPaymentRequestToXml } = require('../conversion')
 const { saveToCrossBorderPaymentEngine } = require('./save-to-cross-border-payment-engine')
 
 const processXbMessage = async (message, receiver) => {
   try {
     const paymentRequest = message.body
-    console.log('Cross Border payment request received:', util.inspect(paymentRequest, false, null, true))
+    console.log('Cross Border payment request received:', { frn: paymentRequest.frn, invoiceNumber: paymentRequest.invoiceNumber })
     const { id, convertedPaymentRequest } = convertPaymentRequestToXml(paymentRequest)
     await saveToCrossBorderPaymentEngine(id, convertedPaymentRequest)
     await receiver.completeMessage(message)
--- app/responses/process-responses.js
+++ app/responses/process-responses.js
@@ -1,4 +1,3 @@
-const util = require('util')
 const sql = require('mssql')
 const { databaseConfig } = require('../config')
 const { getPendingPaymentRequests } = require('./get-pending-payment-requests')
@@ -15,7 +14,7 @@ const processResponses = async () => {
     for (const pendingPaymentRequest of pendingPaymentRequests) {
       try {
         const convertedPaymentRequest = await convertPaymentRequestToJson(pendingPaymentRequest.paymentRequest)
-        console.log('Cross Border payment response received:', util.inspect(convertedPaymentRequest, false, null, true))
+        console.log('Cross Border payment response received:', { frn: convertedPaymentRequest.frn, invoiceNumber: convertedPaymentRequest.invoiceNumber })
         await sendMessage(convertedPaymentRequest)
         await completeCrossBorderPaymentEngine(pendingPaymentRequest.id, transaction)
       } catch (err) {
